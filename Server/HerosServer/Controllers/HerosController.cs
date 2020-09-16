ae3e68eb1f99358a154f45f26c009f7a41ff3724
Doug Fresh <doug.fresh@visanow.com>
2020-09-16 06:48:36 -0500

    Adding in server side

---
+++ Server/HerosServer/Controllers/HerosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HerosServer.Controllers
+{
+    [Route("api/hero")]
+    public class HerosController : Controller
+    {
+        public DbModel Context { get; set; }
+
+        public HerosController()
+        {
+            Context = new DbModel();
+        }
+    }
+}
